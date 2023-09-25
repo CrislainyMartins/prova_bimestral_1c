3c5cf9c651e179bf8d3568b17df2f47bf2cbf2bd
henrique <josehenriquedafavela@gmail.com>
2023-09-25 08:43:02 -0300

    bimestral
    belmiro e crislainy finalizaram a prova

---
+++ prova.py
@@ -0,0 +1,11 @@
+nome= input('digite seu nome: ')#usuário digita o nome 
+idade= int(input('digite seu idade: '))#usuário digita sua idade
+peso= float(input('digite seu peso: '))#usuário digita seu peso
+altura= float(input('digite seu altura: '))#usuário digita sua altura
+#abaixo deve aparecer os dados do usuário
+print('olá ', nome)
+print('sua idade é ' ,idade)
+print('seu peso é ', peso)
+print('sua altura é',altura )
+
+
